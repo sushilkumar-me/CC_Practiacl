e7a0fd07c3bd3e0c0ba32bcc0dc30ee1779d9117
thesushilkumar17@gmail.com <thesushilkumar17@gmail.com>
2026-04-18 11:13:06 +0530

    First Practical

--- PRAC_1.py
+++ PRAC_1.py
@@ -1,1 +1,38 @@
-print("First Practical")
+"""
+Write a program for implementing a MINSTACK,
+which should support operations like push, pop,
+overflow, underflow, display
+"""
+
+class MinStack: 
+    def __init__(self, n): 
+        self.s = [] 
+        self.min = [] 
+        self.n = n 
+    
+    def push(self, x): 
+        if len(self.s) == self.n: 
+            print("Overflow")
+        else: 
+            self.s.append(x)
+            if not self.min or x <= self.min[-1]: 
+                self.min.append(x) 
+    
+    def pop(self): 
+        if not self.s: 
+            print("Underflow")
+        else: 
+            x = self.s.pop()
+            if x == self.min[-1]:
+                self.min.pop()
+    def display(self): 
+        print(self.s)
+        
+
+s = MinStack(5)
+s.push(10)
+s.push(5)
+s.push(20)
+s.display()
+s.pop()
+s.display()
